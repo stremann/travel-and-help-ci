8f0420aa40dfa50cc24fb6ad618c98ecf18aa958
Roman Stremedlovskyi <Roman_Stremedlovskyi@epam.com>
2016-03-25 00:58:35 +0200

    Add rewrite URL

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ app.set( 'port', ( process.env.PORT || 3000 ) );
 app.use( express.static( __dirname + '/www' ) );
 
 app.set( 'views', __dirname + '/www' );
+app.use( '/img',  express.static( path.join( __dirname, 'www/img' ) ) );
 
 app.get( '/', function( request, response ) {
     response.render( 'index' );
